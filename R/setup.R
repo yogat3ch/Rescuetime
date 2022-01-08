7a90ed375a7bc9f001aff516952c2185bb6f0dc2
Stephen Holsenbeck <sholsen@alumni.emory.edu>
2022-01-08 08:36:09 -0500

    improvements to setup

--- R/setup.R
+++ R/setup.R
@@ -1,7 +1,7 @@
 #' @title Setup Rescuetime by adding the RESCUETIME_KEY environmental variable
 #' @description
-#' @param key Your key. See \href{https://www.rescuetime.com/anapi/manage}{API Key Management}
-#' @param .Renviron_path Path to the _.Renviron_ file to modify. Defaults to current directory. If one does not exist it will be created.
+#' @param key \code{(character)} Your key. See \href{https://www.rescuetime.com/anapi/manage}{API Key Management}
+#' @param \code{(character)} .Renviron_path Path to the _.Renviron_ file to modify. Defaults to current directory. If one does not exist it will be created.
 #' @return Messages or warnings as is suitable
 #' @export
 #'
@@ -13,14 +13,16 @@
 #' )
 setup <- function(key = "[Your Key]", .Renviron_path = ".Renviron") {
   key_val <- "RESCUETIME_KEY"
-  if (!file.exists(.Renviron_path)) {
-    file.create(.Renviron_path)
-    rlang::warn(paste0(.Renviron_path, " created"))
+  .rpath <- path.expand(.Renviron_path)
+  l <- readLines(.rpath)
+  if (!file.exists(.rpath)) {
+    file.create(.rpath)
+    rlang::warn(paste0(.rpath, " created"))
   }
 
-  if (nchar(Sys.getenv(key_val)) == 0) {
-    write(paste0(key_val," = '",key,"'"), .Renviron_path, append = TRUE)
-    readRenviron(.Renviron_path)
+  if (!any(stringr::str_detect(l, key_val))) {
+    write(paste0(key_val," = '",key,"'"), .rpath, append = TRUE)
+    readRenviron(.rpath)
     if (nchar(Sys.getenv(key_val)) != 0)
       message("Key successfully set.")
   } else
